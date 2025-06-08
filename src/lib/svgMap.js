386404f6d69c6c85af8be12a84f8df7c399a3554
savwilliams <wil24021@byui.edu>
2025-06-07 20:59:50 -0600

    updated svgMap

--- src/lib/svgMap.js
+++ src/lib/svgMap.js
@@ -1,19 +1,58 @@
 // Houses
-import BasicHouse from '../assets/homeScreen/houses/basicHouse.svg';
+import BasicHouse from '../assets/images/house/house.svg';
+import HouseThumbnail from '../assets/images/house/house_thumbnail.svg'
 
 // Backgrounds
-import HomeScreenYard from '../assets/homeScreen/homeScreenYard.svg';
+import HomeScreenYard from '../assets/images/homeScreen/homeScreenYard.svg';
+import BasicBedroom from '../assets/images/house/basic_bedroom.svg';
+import BlueBackground from '../assets/images/tutorial/blue_background.svg';
 
-//Decor
-import BasicBedroom from '../assets/decor/basicBedroom.svg';
+// Decor
+import Bed from '../assets/images/decor/bed.svg';
+
+// Avatar
+import BasicAvatar from '../assets/images/avatar/basic_avatar';
+import Dress from '../assets/images/avatar/dress.svg';
+import Pants from '../assets/images/avatar/pants.svg';
+import Shirt from '../assets/images/avatar/shirt.svg';
+import LongHair from '../assets/images/avatar/long_hair.svg';
+import ShortHair from '../assets/images/avatar/short_hair';
+
+// HomeScreen
+import Bike from '../assets/images/homeScreen/bike.svg';
+import Sun from '../assets/images/homeScreen/sun.svg';
+
+// Tasks
+import Coin from '../assets/images/tasks/coin.svg';
+
+/////////////////////////////////////////////////////////////////
 
 export const decorMap = {
   // Houses
   basicHouse: BasicHouse,
+  houseThumbnail: HouseThumbnail,
 
   // Backgrounds
   homeScreenYard: HomeScreenYard,
+  basicBedroom: BasicBedroom,
+  blueBackground: BlueBackground,
 
-  //Decor
-  basicBedroom: BasicBedroom
+  // Decor
+  bed: Bed,
+  
+  // Avatar
+  basicAvatar: BasicAvatar,
+  dress: Dress,
+  pants: Pants,
+  shirt: Shirt,
+  longHair: LongHair,
+  shortHair: ShortHair,
+
+  // Homescreen
+  bike: Bike,
+  sun: Sun,
+
+  // Tasks
+  coin: Coin,
+
 };
